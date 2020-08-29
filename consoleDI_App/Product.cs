9f251cb73efba795a006e41666ba9b13478d6072
DESKTOP-HSOL68M\darsh <darshana2000@gmail.com>
2020-08-29 11:35:24 +0800

    Add project files.

---
+++ consoleDI_App/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consoleDI_App
+{
+    public enum Product
+    {
+        keyboard=0,
+        Mouse=1,
+        Speakers=1
+    }
+}
---
+++ consoleDI_App/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace consoleDI_App
+{
+    class Program
+    {
+        public Program()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            var product = string.Empty;
+
+            while (product != "exit")
+            {
+                Console.WriteLine("Enter product:");
+
+            }
+            Console.WriteLine("Hello World!");
+
+        }
+
+        private static void RegisterServices()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
